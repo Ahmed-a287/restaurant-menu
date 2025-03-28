5fbaebdc8e3aa29668c0974255e82092a8bbc742
Ahmed Abbas <ahmed_abbas87@hotmail.com>
2025-03-28 20:32:03 +0100

    Removed old/unused fetch

--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -1,36 +1,9 @@
 'use client';
-import { useEffect, useState } from 'react';
 import Menu from '../../Components/Menu';
 import Navbar from '../../Components/Navbar';
 import Footer from '../../Components/Footer';
 
-export default function Home() {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchMenuItems = async () => {
-      try {
-        const response = await fetch(
-          'https://www.themealdb.com/api/json/v1/1/random.php'
-        );
-        if (!response.ok) {
-          throw new Error('Failed to fetch menu');
-        }
-        const data = await response.json();
-      } catch (err) {
-        setError(err instanceof Error ? err.message : 'Unknown error');
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchMenuItems();
-  }, []);
-
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
-
+export default function MenuPage() {
   return (
     <div>
       <Navbar />
