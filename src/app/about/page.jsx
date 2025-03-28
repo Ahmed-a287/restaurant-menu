f8ec8a5a565c16d5b77a606fbf4df6030eee6113
Ahmed Abbas <ahmed_abbas87@hotmail.com>
2025-03-29 00:56:25 +0100

    Fixed the import with right casing. Deploy to GitHub Pages

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,7 @@
 import styles from '../../styles/about.module.scss';
 import Image from 'next/image';
-import Navbar from '../../components/Navbar';
-import Footer from '../../components/Footer';
+import Navbar from '../../Components/Navbar';
+import Footer from '../../Components/Footer';
 import {
   FaPhoneAlt,
   FaEnvelope,
