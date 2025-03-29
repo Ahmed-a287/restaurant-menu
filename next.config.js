2e3bef5e92c3ff39df6f6453d4622f2701924452
Ahmed Abbas <ahmed_abbas87@hotmail.com>
2025-03-29 02:01:10 +0100

    Applying token

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isGithubPages = process.env.GITHUB_PAGES === 'true';
 
 module.exports = {
   eslint: { ignoreDuringBuilds: true },
-  output: 'export', // Required for static export
-  basePath: isGithubPages ? '/restaurant-menu' : '', // Replace with your repo name
-  assetPrefix: isGithubPages ? '/restaurant-menu/' : '', // Same as above
+  output: 'export',
+  basePath: isGithubPages ? '/restaurant-menu' : '',
+  assetPrefix: isGithubPages ? '/restaurant-menu/' : '',
 };
