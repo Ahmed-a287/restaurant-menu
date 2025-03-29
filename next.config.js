5a266f685ea8362d6ae827d3707a7e7c2013de39
Ahmed Abbas <ahmed_abbas87@hotmail.com>
2025-03-29 02:32:26 +0100

    Remove Static Export

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 const isGithubPages = process.env.GITHUB_PAGES === 'true';
 
-module.exports = {
-  eslint: { ignoreDuringBuilds: true },
-  output: 'export',
-  basePath: isGithubPages ? '/restaurant-menu' : '',
-  assetPrefix: isGithubPages ? '/restaurant-menu/' : '',
-};
+module.exports = {};
